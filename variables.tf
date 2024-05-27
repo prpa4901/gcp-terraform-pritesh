a6c8c99e107cb6d4249f4aae8a588822cbd2e056
Pritesh Laxman Patil <priteshlaxmanpatil@Priteshs-MacBook-Pro.local>
2024-05-26 21:56:21 -0400

    first commit3

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "GOOGLE_CREDENTIALS" {
+  description = "The creds ID"
+  type        = string
+}
+
+variable "project" {
+  description = "The project ID in Google Cloud"
+  type        = string
+}
+
+variable "region" {
+  description = "Region of the google cloud"
+  type        = string
+}
